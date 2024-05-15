99ec1e3bf59405098f982cb7ff3df60db23eb347
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-05-15 21:25:31 +0900

    modify create_graph_dataeset.py

--- scripts/create_graph_dataset.py
+++ scripts/create_graph_dataset.py
@@ -7,11 +7,8 @@ from utils import yaml_handler
 
 parser = argparse.ArgumentParser()
 parser.add_argument("data_root", type=str)
-parser.add_argument("feature_type", type=str)
-parser.add_argument(
-    "-c", "--config", type=str, required=False, default="configs/model_config.yaml"
-)
+parser.add_argument("config", type=str)
 args = parser.parse_args()
 
 config = yaml_handler.load(args.config)
-DynamicSpatialTemporalGraphDataset(args.data_root, args.feature_type, config, force_reload=True)
+DynamicSpatialTemporalGraphDataset(args.data_root, config.dataset, force_reload=True)
