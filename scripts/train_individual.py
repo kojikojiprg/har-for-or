e1674ab7f19c1bf6253b1f078b5ec5d0704eae06
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-05-31 20:50:29 +0900

    update data loader

--- scripts/train_individual.py
+++ scripts/train_individual.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     model_cfg = yaml_handler.load(model_cfg_path)
 
     # load dataset
-    dataset = load_dataset(data_root, "individual", data_type, dataset_cfg, model_cfg.batch_size, True)
-    datamodule = DataModule(dataset, model_cfg.num_workers)
+    dataset = load_dataset(data_root, "individual", data_type, dataset_cfg, True)
+    datamodule = DataModule(dataset, model_cfg.batch_size, model_cfg.num_workers)
 
     # create model
     model = IndividualActivityRecognition(model_cfg)
--- src/data/datamodule.py
+++ src/data/datamodule.py
@@ -1,16 +1,55 @@
 from lightning.pytorch import LightningDataModule
+from torch_geometric.data import DataLoader
 from webdataset import WebDataset, WebLoader
 
 
 class DataModule(LightningDataModule):
-    def __init__(self, dataset: WebDataset, num_workers: int = 8):
+    def __init__(
+        self,
+        dataset: WebDataset,
+        dataset_type: str,
+        batch_size: int,
+        num_workers: int = 8,
+    ):
         super().__init__()
         self.prepare_data_per_node = True
         self.dataset = dataset
+        self.dataset_type = dataset_type
+        self.batch_size = batch_size
         self.num_workers = num_workers
 
     def train_dataloader(self):
-        return WebLoader(self.dataset, num_workers=self.num_workers, pin_memory=True)
+        if self.dataset_type == "individual":
+            return WebLoader(
+                self.dataset,
+                batch_size=self.batch_size,
+                num_workers=self.num_workers,
+                pin_memory=True,
+            )
+        elif self.dataset_type == "group":
+            return DataLoader(
+                self.dataset,
+                batch_size=self.batch_size,
+                num_workers=self.num_workers,
+                pin_memory=True,
+            )
+        else:
+            raise ValueError
 
     def predict_dataloader(self):
-        return WebLoader(self.dataset, num_workers=self.num_workers, pin_memory=True)
+        if self.dataset_type == "individual":
+            return WebLoader(
+                self.dataset,
+                batch_size=self.batch_size,
+                num_workers=self.num_workers,
+                pin_memory=True,
+            )
+        elif self.dataset_type == "group":
+            return DataLoader(
+                self.dataset,
+                batch_size=self.batch_size,
+                num_workers=self.num_workers,
+                pin_memory=True,
+            )
+        else:
+            raise ValueError
