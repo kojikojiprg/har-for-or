5979973569811eba4caf5566cbea2e99958ea690
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-06-11 14:29:30 +0900

    update writing shard

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,13 +2,10 @@ import functools
 import gc
 import itertools
 import os
-import queue
 import sys
 import time
 import warnings
 from glob import glob
-from multiprocessing import shared_memory
-from multiprocessing.managers import SyncManager
 from types import SimpleNamespace
 
 warnings.filterwarnings("ignore")
@@ -20,6 +17,7 @@ from tqdm import tqdm
 from src.model import HumanTracking
 from src.utils import json_handler, video
 
+from .obj import ShardWritingManager, SharedNDArray, SharedShardWriter
 from .transform import (
     FlowToTensor,
     FrameToTensor,
@@ -160,61 +158,6 @@ def write_shards(
         pbar_ht.close()
         pbar_w.close()
     gc.collect()
-
-
-class SharedNDArray:
-    def __init__(self, name, shape, dtype):
-        self.name = name
-        self.shape = shape
-        self.dtype = dtype
-        self.shm = self.create_shared_memory()
-
-    def calc_ndarray_size(self):
-        return np.prod(self.shape) * np.dtype(self.dtype).itemsize
-
-    def create_shared_memory(self):
-        size = self.calc_ndarray_size()
-        return shared_memory.SharedMemory(name=self.name, create=True, size=size)
-
-    def get_shared_memory(self):
-        shm = shared_memory.SharedMemory(name=self.name)
-        return shm
-
-    def ndarray(self):
-        shm = self.get_shared_memory()
-        return np.ndarray(self.shape, self.dtype, buffer=shm.buf), shm
-
-    def unlink(self):
-        self.shm.close()
-        self.shm.unlink()
-
-
-class SharedShardWriter(wds.ShardWriter):
-    def __init__(self, shard_pattern, maxcount, verbose=0, post=None):
-        super().__init__(shard_pattern, maxcount, verbose=verbose, post=post)
-        self.write_que = queue.Queue()
-        self.finished = False
-
-    # def tar_is_closed(self):
-    #     return self.tarstream is None or self.tarstream.tarstream.closed
-
-    def add_write_que(self, data):
-        self.write_que.put(data)
-
-    def finish_writing(self):
-        self.finished = True
-
-    def write_async(self):
-        while True:
-            self.write(self.write_que.get())
-
-            if self.finished and self.write_que.empty():
-                break  # finish writing
-            time.sleep(0.001)
-
-
-class ShardWritingManager(SyncManager):
-    pass
 
 
 def _check_full(tail_frame, tail_ht, head, que_len):
---
+++ src/data/obj.py
@@ -0,0 +1,62 @@
+import queue
+import time
+from multiprocessing import shared_memory
+from multiprocessing.managers import SyncManager
+
+import numpy as np
+import webdataset as wds
+
+
+class ShardWritingManager(SyncManager):
+    pass
+
+
+class SharedNDArray:
+    def __init__(self, name, shape, dtype):
+        self.name = name
+        self.shape = shape
+        self.dtype = dtype
+        self.shm = self.create_shared_memory()
+
+    def calc_ndarray_size(self):
+        return np.prod(self.shape) * np.dtype(self.dtype).itemsize
+
+    def create_shared_memory(self):
+        size = self.calc_ndarray_size()
+        return shared_memory.SharedMemory(name=self.name, create=True, size=size)
+
+    def get_shared_memory(self):
+        shm = shared_memory.SharedMemory(name=self.name)
+        return shm
+
+    def ndarray(self):
+        shm = self.get_shared_memory()
+        return np.ndarray(self.shape, self.dtype, buffer=shm.buf), shm
+
+    def unlink(self):
+        self.shm.close()
+        self.shm.unlink()
+
+
+class SharedShardWriter(wds.ShardWriter):
+    def __init__(self, shard_pattern, maxcount, verbose=0, post=None):
+        super().__init__(shard_pattern, maxcount, verbose=verbose, post=post)
+        self.write_que = queue.Queue()
+        self.finished = False
+
+    # def tar_is_closed(self):
+    #     return self.tarstream is None or self.tarstream.tarstream.closed
+
+    def add_write_que(self, data):
+        self.write_que.put(data)
+
+    def finish_writing(self):
+        self.finished = True
+
+    def write_async(self):
+        while True:
+            self.write(self.write_que.get())
+
+            if self.finished and self.write_que.empty():
+                break  # finish writing
+            time.sleep(0.001)
