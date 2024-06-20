99df155b02af93ecc4d987a2487f02be082ab15d
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-06-20 17:41:44 +0900

    initial commit

---
+++ scripts/pred_individual.py
@@ -0,0 +1,132 @@
+# import os
+# from glob import glob
+import sys
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from sklearn.manifold import TSNE
+from tqdm import tqdm
+from webdataset import WebLoader
+
+sys.path.append(".")
+from src.data import load_dataset
+from src.model import IndividualActivityRecognition
+from src.utils import video, vis, yaml_handler
+
+if __name__ == "__main__":
+    data_root = "../datasets/dataset03/train/01"
+    # data_dirs = sorted(glob(os.path.join(data_root, "*/")))
+    data_dirs = [data_root]
+    data_dir = data_dirs[0]
+
+    checkpoint_path = (
+        "models/individual/individual-seq_len90-stride30-256x192-loss-min-v2.ckpt"
+    )
+    config = yaml_handler.load("configs/individual.yaml")
+    seq_len = config.seq_len
+    stride = config.stride
+    gpu_id = 0
+    device = f"cuda:{gpu_id}"
+
+    # load dataset
+    dataset, n_samples = load_dataset(data_dirs, "individual", config, False)
+    dataloader = WebLoader(dataset, num_workers=config.num_workers, pin_memory=True)
+
+    # load model
+    model = IndividualActivityRecognition(config)
+    model.configure_model()
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint["state_dict"])
+
+    # load video
+    video_path = f"{data_dir}.mp4"
+    cap = video.Capture(video_path)
+    max_n_frame = cap.frame_count
+    img_size = cap.size
+
+    # create writers
+    wrt_x_vis = video.Writer(f"{data_dir}/pred_x_vis.mp4", cap.fps, cap.size)
+    wrt_x_spc = video.Writer(f"{data_dir}/pred_x_spc.mp4", cap.fps, cap.size)
+    wrt_cluster = video.Writer(f"{data_dir}/pred_cluster.mp4", cap.fps, cap.size)
+
+    # pred
+    mse_x_vis_dict = {}
+    mse_x_spc_dict = {}
+    latent_features = {"label": [], "mu": [], "logvar": [], "z": []}
+
+    model.eval()
+    with torch.no_grad():
+        pre_n_frame = seq_len
+        results_tmp = []
+        for batch in tqdm(dataloader, total=n_samples):
+            result = model.predict_step(batch)
+            n_frame = int(result["key"].split("_")[1])
+            _id = result["id"]
+
+            # collect mse
+            if _id not in mse_x_vis_dict:
+                mse_x_vis_dict[_id] = {}
+                mse_x_spc_dict[_id] = {}
+            mse_x_vis_dict[_id][n_frame] = result["mse_x_vis"]
+            mse_x_spc_dict[_id][n_frame] = result["mse_x_spc"]
+
+            # collect latent features
+            latent_features["label"].append(np.argmax(result["y"]).item())
+            latent_features["mu"].append(result["mu"])
+            latent_features["logvar"].append(result["logvar"])
+
+            # plot bboxs
+            if pre_n_frame < n_frame:
+                for i in range(stride):
+                    n_frame_tmp = pre_n_frame - stride + i
+                    idx_data = seq_len - stride + i
+
+                    frame = cap.read(n_frame_tmp)[1]
+                    frame = cv2.putText(
+                        frame,
+                        f"frame:{n_frame_tmp}",
+                        (10, 40),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        1.0,
+                        (255, 255, 255),
+                        1,
+                    )
+
+                    frame_vis = vis.plot_on_frame(
+                        frame.copy(), results_tmp, idx_data, img_size, "x_vis"
+                    )
+                    wrt_x_vis.write(frame_vis)
+                    frame_spc = vis.plot_on_frame(
+                        frame.copy(), results_tmp, idx_data, img_size, "x_spc"
+                    )
+                    wrt_x_spc.write(frame_spc)
+                    frame_cluster = vis.plot_on_frame(
+                        frame.copy(), results_tmp, idx_data, img_size, "cluster"
+                    )
+                    wrt_cluster.write(frame_cluster)
+
+                results_tmp = []
+                pre_n_frame = n_frame
+
+            # add result in temporary result list
+            results_tmp.append(result)
+
+    del cap, wrt_x_vis, wrt_x_spc, wrt_cluster
+
+    # plot mse
+    vis.plot_mse(mse_x_vis_dict, max_n_frame, stride, f"{data_dir}/pred_x_vis.jpg")
+    vis.plot_mse(mse_x_spc_dict, max_n_frame, stride, f"{data_dir}/pred_x_spc.jpg")
+
+    # plot latent feature
+    tsne = TSNE(n_components=2, random_state=0, perplexity=30, n_iter=1000)
+    mu_embedded = tsne.fit_transform(np.array(latent_features["mu"]))
+    labels = np.array(latent_features["label"])
+    unique_labels = np.unique(labels)
+    for label in unique_labels:
+        mu_cluster = mu_embedded[labels == label]
+        plt.scatter(mu_cluster.T[0], mu_cluster.T[1], s=2, label=label, alpha=0.7)
+    plt.legend()
+    plt.savefig(f"{data_dir}/pred_mu_tsne.jpg", bbox_inches="tight")
+    plt.close()
---
+++ src/utils/vis.py
@@ -0,0 +1,195 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+EDGE_INDEX = [
+    (0, 1),
+    (0, 2),
+    (1, 3),
+    (2, 4),  # Head
+    (5, 6),
+    (5, 7),
+    (7, 9),
+    (6, 8),
+    (8, 10),
+    (5, 11),
+    (6, 12),  # Body
+    (11, 13),
+    (12, 14),
+    (13, 15),
+    (14, 16),
+]
+KP_COLOR = [
+    # Nose, LEye, REye, LEar, REar
+    (0, 255, 255),
+    (0, 191, 255),
+    (0, 255, 102),
+    (0, 77, 255),
+    (0, 255, 0),
+    # LShoulder, RShoulder, LElbow, RElbow, LWrist, RWrist
+    (77, 255, 255),
+    (77, 255, 204),
+    (77, 204, 255),
+    (191, 255, 77),
+    (77, 191, 255),
+    (191, 255, 77),
+    # LHip, RHip, LKnee, Rknee, LAnkle, RAnkle, Neck
+    (204, 77, 255),
+    (77, 255, 204),
+    (191, 77, 255),
+    (77, 255, 191),
+    (127, 77, 255),
+    (77, 255, 127),
+    (0, 255, 255),
+]
+LIMB_COLOR = [
+    (0, 215, 255),
+    (0, 255, 204),
+    (0, 134, 255),
+    (0, 255, 50),
+    (77, 255, 222),
+    (77, 196, 255),
+    (77, 135, 255),
+    (191, 255, 77),
+    (77, 255, 77),
+    (77, 222, 255),
+    (255, 156, 127),
+    (0, 127, 255),
+    (255, 127, 77),
+    (0, 77, 255),
+    (255, 77, 36),
+]
+
+
+def draw_skeleton(frame: np.array, kps: np.array, color: tuple = None):
+    part_line = {}
+
+    # draw keypoints
+    for n in range(len(kps)):
+        cor_x, cor_y = int(kps[n, 0]), int(kps[n, 1])
+        part_line[n] = (cor_x, cor_y)
+        c = KP_COLOR[n] if color is None else color
+        cv2.circle(frame, (cor_x, cor_y), 3, c, 1)
+
+    # draw limbs
+    for start_p, end_p in EDGE_INDEX:
+        if start_p in part_line and end_p in part_line:
+            start_xy = part_line[start_p]
+            end_xy = part_line[end_p]
+            c = LIMB_COLOR[n] if color is None else color
+            cv2.line(frame, start_xy, end_xy, c, 2, 3)
+
+    return frame
+
+
+def draw_bbox(frame: np.array, bbox: np.array, color: tuple):
+    pt1, pt2 = bbox.astype(int).reshape(2, 2)
+    frame = cv2.rectangle(frame, pt1, pt2, color, 2)
+    return frame
+
+
+def plot_on_frame(frame, results, idx_data, img_size, content):
+    cm = plt.get_cmap("tab10")
+    for data in results:
+        _id = data["id"]
+        bbox = data["x_spc"][idx_data]
+        mask = data["mask"][idx_data]
+
+        if mask:
+            continue
+
+        # id
+        pt = tuple(np.mean(bbox, axis=0).astype(int))
+        frame = cv2.putText(
+            frame, str(_id), pt, cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2
+        )
+
+        bbox = (bbox.copy() * img_size) + np.array(img_size) / 2
+        if content == "x_vis":
+            fake_img = data["fake_x_vis"][idx_data]
+            mse_x_vis = data["mse_x_vis"]
+
+            # resize imgs and write
+            x1, y1, x2, y2 = bbox.reshape(4).astype(int)
+            fake_img = cv2.resize(fake_img, (x2 - x1, y2 - y1))
+            fake_img = fake_img[:, :, :3]
+            mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+            fake_img = ((fake_img * std) + mean) * 255
+            frame[y1:y2, x1:x2] = (fake_img).astype(np.uint8)
+
+            # mse
+            pt = tuple(np.min(bbox, axis=0).astype(int))  # top-left
+            frame = cv2.putText(
+                frame,
+                f"{mse_x_vis:.3f}",
+                pt,
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.8,
+                (255, 255, 255),
+                2,
+            )
+        elif content == "x_spc":
+            mse_x_spc = data["mse_x_spc"]
+            fake_bbox = data["fake_x_spc"][idx_data]
+
+            # bbox and fake_bbox
+            fake_bbox = (fake_bbox.copy() * img_size) + np.array(img_size) / 2
+            frame = draw_bbox(frame, bbox, color=(0, 255, 0))
+            frame = draw_bbox(frame, fake_bbox, color=(0, 0, 255))
+
+            # mse
+            pt = tuple(np.min(bbox, axis=0).astype(int))  # top-left
+            frame = cv2.putText(
+                frame,
+                f"{mse_x_spc:.3f}",
+                pt,
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.8,
+                (255, 255, 255),
+                2,
+            )
+        elif content == "cluster":
+            label = str(np.argmax(data["y"]))
+
+            color = (np.array(cm(int(label))[:3]) * 255).astype(int).tolist()
+            color = tuple(color[::-1])  # RGB -> BGR
+            frame = draw_bbox(frame, bbox, color)
+
+            # clustering label
+            pt = tuple(np.min(bbox, axis=0).astype(int))  # top-left
+            frame = cv2.putText(
+                frame, label, pt, cv2.FONT_HERSHEY_COMPLEX, 0.8, color, 2
+            )
+        else:
+            raise ValueError
+
+    return frame
+
+
+def plot_mse(mse_x_dict, frame_count, stride, figpath):
+    vals_dict = {}
+    for _id, mse_dict in mse_x_dict.items():
+        if len(mse_dict) < 2:
+            continue
+        n_frames = np.array(list(mse_dict.keys()))
+        mses = np.array(list(mse_dict.values()))
+
+        idxs = n_frames // stride
+        n_samples = frame_count // stride + 1
+        vals = np.full((n_samples,), np.nan, np.float32)
+        vals[idxs] = mses
+
+        vals_dict[_id] = vals
+
+    ids = list(vals_dict.keys())
+    vals = np.array(list(vals_dict.values())).T
+    mse_mean = np.nanmean(vals, axis=1)
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(vals, color="black", linewidth=1, alpha=0.5, label=ids)
+    plt.plot(mse_mean, color="red", label="mean")
+    # plt.legend()
+    plt.xlim(0, n_samples)
+    plt.xlabel("sec")
+    plt.savefig(figpath, bbox_inches="tight")
+    plt.close()
