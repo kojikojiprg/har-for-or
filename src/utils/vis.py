1b78e1acb71afaed8b989aea26a7193f407ee3d9
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-07-23 15:34:00 +0900

    modify vis clustering

--- src/utils/vis.py
+++ src/utils/vis.py
@@ -162,8 +162,7 @@ def plot_on_frame(frame, results, idx_data, frame_size, content):
                 2,
             )
         elif content == "cluster":
-            # label = str(data["label"])
-            label = str(data["y_argmax"])
+            label = str(data["label"])
 
             color = (np.array(cm(int(label))[:3]) * 255).astype(int).tolist()
             color = tuple(color[::-1])  # RGB -> BGR
