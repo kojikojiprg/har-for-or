0223311d8c6d1e996cdf4767bd35615436cf65fc
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-09-25 10:31:46 +0900

    update sqvae of clustering loss

--- src/model/individual/sqvae.py
+++ src/model/individual/sqvae.py
@@ -203,12 +203,20 @@ class SQVAE(LightningModule):
 
             mask_supervised = np.isin(keys, self.annotations.T[0]).ravel()
             mask_supervised = torch.tensor(mask_supervised).to(self.device)
-            # lc = F.cross_entropy(c_prob, psuedo_labels_prob, reduction="none")
-            lc = (c_prob * (c_prob.log() - psuedo_labels_prob.log())).sum(dim=-1)
-            lc_real = (lc.detach() * mask_supervised).mean()
-            lc_psuedo = (lc.detach() * ~mask_supervised).mean()
+            if torch.any(mask_supervised):
+                lc_real = F.cross_entropy(
+                    c_prob[mask_supervised], psuedo_labels_prob[mask_supervised]
+                )
+                loss_dict["c"] = lc_real.item()
+            else:
+                lc_real = 0.0
+                loss_dict["c"] = 0.0
+            lc_psuedo = c_prob[~mask_supervised] * (
+                c_prob[~mask_supervised].log()
+                - psuedo_labels_prob[~mask_supervised].log()
+            )
+            lc_psuedo = lc_psuedo.mean()
             lc = lc_real * 10 + lc_psuedo * 0.01
-            loss_dict["c"] = lc_real.item()
             loss_dict["c_psuedo"] = lc_psuedo.item()
 
         loss_total = (
