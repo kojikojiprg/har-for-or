58d845378278c811bac96864fd13949b66dfeebe
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-11-08 08:53:08 +0900

    update annotation module

--- src/data/annotation.py
+++ src/data/annotation.py
@@ -18,67 +18,66 @@ def load_annotation_train(
     config: SimpleNamespace,
     seed: int = 42,
 ):
-    np.random.seed(seed)
 
-    # load annotation
-
-    annotation = []
-    n_samples_lst = []
+    used_annotation = []
     for data_root in data_root_lst:
         # load annotation
         path = f"{data_root}/annotation/role_train.txt"
-        annotation += np.loadtxt(path, str, skiprows=1).tolist()
+        annotation = np.loadtxt(path, str, skiprows=1)
 
-        n_samples_lst += count_n_samples(data_root, config)
-    annotation = np.array(annotation)
+        n_samples_lst = count_n_samples(data_root, config)
 
-    # count labels
-    counts_dict = {i: {} for i in range(config.n_clusters)}
-    total = 0
-    for key, count in n_samples_lst:
-        count = int(count)
-        ann = annotation[annotation.T[0] == key]
-        total += count
-        if len(ann) == 1:
-            label = int(ann[0, 1])
-            if key not in counts_dict[label]:
-                counts_dict[label][key] = 0
-            counts_dict[label][key] += count
-        elif len(ann) == 0:
-            pass
-        else:
-            print("warning", key, len(ann))
+        # count labels
+        total = 0
+        counts_dict = {i: {} for i in range(config.n_clusters)}
+        for key, count in n_samples_lst:
+            count = int(count)
+            ann = annotation[annotation.T[0] == key]
+            total += count
+            if len(ann) == 1:
+                label = int(ann[0, 1])
+                if key not in counts_dict[label]:
+                    counts_dict[label][key] = 0
+                counts_dict[label][key] += count
+            elif len(ann) == 0:
+                pass
+            else:
+                print("warning", key, len(ann))
 
-    # sum label counts until min_n_samples
-    used_annotation = []
-    summary_annotation = []
-    count_non_labeled = total
-    for label, counts in counts_dict.items():
-        # sort counts by keys
-        counts = sorted([(key, c) for key, c in counts.items()], key=lambda x: x[0])
+        if data_root[-1] == "/":
+            data_root = data_root[:-1]
+        dataset_name = os.path.basename(data_root)
 
-        # shuffle
-        counts = np.array(counts)
-        indices = np.random.choice(len(counts), len(counts), replace=False)
-        counts = counts[indices]
+        # sum label counts until min_n_samples
+        summary_annotation = []
+        count_non_labeled = total
+        for label, counts in counts_dict.items():
+            # sort counts by keys
+            counts = sorted([(key, c) for key, c in counts.items()], key=lambda x: x[0])
 
-        # sum counts
-        count_sum = 0
-        for k, c in counts:
-            used_annotation.append((k, label))
-            count_sum += int(c)
-            count_non_labeled -= int(c)
-            if count_sum >= config.min_n_labeled_samples:
-                summary_annotation.append((label, count_sum))
-                break
+            # shuffle
+            np.random.seed(seed)
+            counts = np.array(counts)
+            indices = np.random.choice(len(counts), len(counts), replace=False)
+            counts = counts[indices]
 
-    summary_annotation.append(("non-labeld", count_non_labeled))
-    summary_annotation.append(("total", total))
+            # sum counts
+            count_sum = 0
+            for k, c in counts:
+                used_annotation.append((k, label))
+                count_sum += int(c)
+                count_non_labeled -= int(c)
+                if count_sum >= config.min_n_labeled_samples:
+                    summary_annotation.append((label, count_sum))
+                    break
 
-    # save sammary into checkpoint directory
-    path = f"{checkpoint_dir}/annotation_train_summary.tsv"
-    if not os.path.exists(path):
-        np.savetxt(path, summary_annotation, "%s", delimiter="\t")
+        summary_annotation.append(("non-labeld", count_non_labeled))
+        summary_annotation.append(("total", total))
+
+        # save sammary into checkpoint directory
+        path = f"{checkpoint_dir}/annotation_train_summary_{dataset_name}.tsv"
+        if not os.path.exists(path):
+            np.savetxt(path, summary_annotation, "%s", delimiter="\t")
 
     # save used annotation into checkpoint directory
     path = f"{checkpoint_dir}/annotation_train.tsv"
