d05db8768668aa2ed6ef9b9ca92862c5e0c27eb8
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-12-05 12:49:51 +0900

    wip

---
+++ src/model/individual/modules_todo.py
@@ -0,0 +1,380 @@
+from types import SimpleNamespace
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from src.model.layers import MLP  # , TransformerEncoderBlock
+
+
+def get_n_pts(config: SimpleNamespace):
+    if not config.mask_leg:
+        n_pts = 17 + 2
+    else:  # mask ankles and knees
+        n_pts = 17 - 4 + 2
+    return n_pts
+
+
+def gumbel_softmax_relaxation(logits, temperature, eps=1e-10):
+    U = torch.rand(logits.shape, device=logits.device)
+    g = -torch.log(-torch.log(U + eps) + eps)
+    y = logits + g
+    return F.softmax(y / temperature, dim=-1)
+
+
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self, ndim, nheads, dropout):
+        super().__init__()
+        self.nheads = nheads
+        self.attn = nn.MultiheadAttention(
+            ndim, nheads, dropout=dropout, batch_first=True
+        )
+        self.norm1 = nn.LayerNorm(ndim)
+
+        self.ff = MLP(ndim)
+        self.dropout2 = nn.Dropout(dropout)
+        self.norm2 = nn.LayerNorm(ndim)
+
+    def forward(self, x, need_weights=False):
+        b, seq_len = x.size()[:2]
+        # x (b * seq_len, npatch, ndim)
+        x_attn, attn_w = self.attention_block(x, need_weights)
+        x = self.norm1(x + x_attn)
+        x = self.norm2(x + self.feed_forward_block(x))
+
+        return x, attn_w
+
+    def attention_block(self, x, need_weights):
+        x, attn_w = self.attn(x, x, x, need_weights=need_weights)
+        return x, attn_w
+
+    def feed_forward_block(self, x):
+        x = self.ff(x)
+        x = self.dropout2(x)
+        return x
+
+
+class Embedding(nn.Module):
+    def __init__(self, config, n_pts):
+        super().__init__()
+        self.seq_len = config.seq_len
+        self.n_pts = n_pts
+        self.latent_ndim = config.latent_ndim
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(2, config.latent_ndim // 8, 6, 3),
+            nn.GroupNorm(1, config.latent_ndim // 8),
+            nn.SiLU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(config.latent_ndim // 8, config.latent_ndim // 4, 5, 3),
+            nn.GroupNorm(1, config.latent_ndim // 4),
+            nn.SiLU(),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(config.latent_ndim // 4, config.latent_ndim // 2, 5, 2),
+            nn.GroupNorm(1, config.latent_ndim // 2),
+            nn.SiLU(),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv1d(config.latent_ndim // 2, config.latent_ndim, 3, 1),
+            nn.GroupNorm(1, config.latent_ndim),
+            nn.SiLU(),
+        )
+
+    def forward(self, x):
+        # x (b, seq_len, n_pts, 2)
+        x = x.permute(0, 2, 3, 1).contiguous()  # (b, n_pts, 2, seq_len)
+        x = x.view(-1, 2, self.seq_len)  # (b * n_pts, 2, seq_len)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)  # (b * n_pts, ndim, 1)
+        x = x.view(-1, self.n_pts, self.latent_ndim)
+        return x
+
+
+class ClassificationHead(nn.Module):
+    def __init__(self, config: SimpleNamespace):
+        super().__init__()
+        self.emb_kps = Embedding(config, 13)
+        self.emb_bbox = Embedding(config, 2)
+
+        self.cls_token = nn.Parameter(torch.randn(1, 1, config.latent_ndim))
+        self.encoders = nn.ModuleList(
+            [
+                TransformerEncoderBlock(
+                    config.latent_ndim, config.nheads, config.dropout
+                )
+                for _ in range(config.nlayers_cls)
+            ]
+        )
+        self.mlp = MLP(config.latent_ndim, config.n_clusters)
+
+        log_param_q_cls = np.log(config.param_q_cls_init)
+        self.log_param_q_cls = nn.Parameter(
+            torch.tensor(log_param_q_cls, dtype=torch.float32)
+        )
+
+    def forward(self, kps, bbox, is_train):
+        # embedding
+        kps = self.emb_kps(kps)
+        bbox = self.emb_bbox(bbox)
+        z = torch.cat([kps, bbox], dim=1)
+        # z (b, n_pts, latent_ndim)
+
+        # concat cls_token
+        cls_token = self.cls_token.repeat(z.size(0), 1, 1)
+        z = torch.cat([cls_token, z], dim=1)
+        # z (b, 1 + n_pts, latent_ndim)
+
+        if is_train:
+            for layer in self.encoders:
+                z, attn_w = layer(z)
+            attn_w_tensor = None
+        else:
+            attn_w_lst = []
+            for layer in self.encoders:
+                z, attn_w = layer(z, need_weights=True)
+                attn_w_lst.append(attn_w.unsqueeze(1))
+            attn_w_tensor = torch.cat(attn_w_lst, dim=1)
+        # z (b, 1 + n_pts, latent_ndim)
+
+        logits = self.mlp(z[:, 0, :])  # (b, n_clusters)
+
+        # param_q = 1 + self.log_param_q_cls.exp()
+        param_q = self.log_param_q_cls.exp()  # NOTE
+        precision_q_cls = 0.5 / torch.clamp(param_q, min=1e-10)
+        logits = logits * precision_q_cls
+
+        return logits, attn_w_tensor
+
+
+class Encoder(nn.Module):
+    def __init__(self, config: SimpleNamespace):
+        super().__init__()
+        self.latent_ndim = config.latent_ndim
+        # self.emb = Embedding(config)
+        self.emb_kps = Embedding(config, 13)
+        self.emb_bbox = Embedding(config, 2)
+        self.emb_c = MLP(config.n_clusters, config.latent_ndim)
+        self.norm_c = nn.LayerNorm(config.latent_ndim)
+
+        self.encoders = nn.ModuleList(
+            [
+                TransformerEncoderBlock(
+                    config.latent_ndim, config.nheads, config.dropout
+                )
+                for _ in range(config.nlayers)
+            ]
+        )
+
+    def forward(self, kps, bbox, c_probs, is_train):
+        # embedding
+        # x = torch.cat([kps, bbox], dim=2)
+        # z = self.emb(x)
+        kps = self.emb_kps(kps)
+        bbox = self.emb_bbox(bbox)
+        z = torch.cat([kps, bbox], dim=1)
+        # z (b, n_pts, latent_ndim)
+
+        # add c_probs
+        c_probs = self.emb_c(c_probs).view(-1, 1, self.latent_ndim)
+        z = self.norm_c(z + c_probs)
+
+        if is_train:
+            for layer in self.encoders:
+                z, attn_w = layer(z)
+            attn_w_tensor = None
+        else:
+            attn_w_lst = []
+            for layer in self.encoders:
+                z, attn_w = layer(z, need_weights=True)
+                attn_w_lst.append(attn_w.unsqueeze(1))
+            attn_w_tensor = torch.cat(attn_w_lst, dim=1)
+        # z (b, n_pts, latent_ndim)
+
+        return z, attn_w_tensor
+
+
+class GaussianVectorQuantizer(nn.Module):
+    def __init__(self, config: SimpleNamespace):
+        super().__init__()
+        self.book_size = config.book_size
+
+        mu = []
+        for i in range(config.n_clusters):
+            mu.append(torch.randn(1, config.latent_ndim))
+        self.books = nn.ParameterList(
+            [
+                nn.Parameter(torch.randn(self.book_size, config.latent_ndim) + mu[i])
+                for i in range(config.n_clusters)
+            ]
+        )
+
+        # self.books = nn.ParameterList(
+        #     [
+        #         nn.Parameter(torch.randn(self.book_size, config.latent_ndim))
+        #         for i in range(config.n_clusters)
+        #     ]
+        # )
+
+        log_param_q = np.log(config.param_q_init)
+        self.log_param_q = nn.Parameter(torch.tensor(log_param_q, dtype=torch.float32))
+
+    def calc_distance(self, z, book):
+        distances = (
+            torch.sum(z**2, dim=1, keepdim=True)
+            + torch.sum(book**2, dim=1)
+            - 2 * torch.matmul(z, book.t())
+        )
+        return distances
+
+    def forward(self, ze, c_probs, temperature, is_train):
+        # ze (b, n_pts, latent_ndim)
+        b, n_pts, latent_ndim = ze.size()
+
+        # param_q = 1 + self.log_param_q.exp()
+        param_q = self.log_param_q.exp()  # NOTE
+        precision_q = 0.5 / torch.clamp(param_q, min=1e-10)
+
+        # if is_train:
+        #     logits = torch.empty((0, n_pts, self.book_size)).to(ze.device)
+        #     zq = torch.empty((0, n_pts, latent_ndim)).to(ze.device)
+        #     books = torch.cat(list(self.books.parameters()), dim=0)
+        #     books = books.view(-1, self.book_size, latent_ndim)
+        #     for batch_idx, idx in enumerate(c_probs.argmax(dim=-1)):
+        #         # compute logits and zq of all books
+        #         zei = ze[batch_idx]
+        #         logit = -self.calc_distance(zei, books[idx]) * precision_q
+        #         encoding = gumbel_softmax_relaxation(logit, temperature)
+        #         zqi = torch.matmul(encoding, books[idx])
+
+        #         logits = torch.cat(
+        #             [logits, logit.view(1, n_pts, self.book_size)], dim=0
+        #         )
+        #         zq = torch.cat([zq, zqi.view(1, n_pts, latent_ndim)], dim=0)
+        #         # mean_prob = torch.mean(prob.detach(), dim=0)
+
+        if is_train:
+            logits = torch.empty((0, n_pts, self.book_size)).to(ze.device)
+            zq = torch.empty((0, n_pts, latent_ndim)).to(ze.device)
+            books = torch.cat(list(self.books.parameters()), dim=0)
+            books = books.view(-1, self.book_size, latent_ndim)
+            for batch_idx, c_prob in enumerate(c_probs):
+                # compute logits and zq of all books
+                zei = ze[batch_idx]
+                zqi = torch.zeros_like(zei)
+                logit = torch.zeros((n_pts, self.book_size)).to(ze.device)
+                for j, book in enumerate(books):
+                    logitj = -self.calc_distance(zei, book) * precision_q
+                    logit = logit + logitj * c_prob[j]
+                    encoding = gumbel_softmax_relaxation(logitj, temperature)
+                    zqi = zqi + torch.matmul(encoding, book) * c_prob[j]
+
+                logits = torch.cat(
+                    [logits, logit.view(1, n_pts, self.book_size)], dim=0
+                )
+                zq = torch.cat([zq, zqi.view(1, n_pts, latent_ndim)], dim=0)
+                # mean_prob = torch.mean(prob.detach(), dim=0)
+        else:
+            logits = torch.empty((0, n_pts, self.book_size)).to(ze.device)
+            books = torch.empty((0, self.book_size, latent_ndim)).to(ze.device)
+            for batch_idx, idx in enumerate(c_probs.argmax(dim=-1)):
+                book = self.books[idx]
+                logit = -self.calc_distance(ze[batch_idx], book) * precision_q
+
+                logits = torch.cat(
+                    [logits, logit.view(1, n_pts, self.book_size)], dim=0
+                )
+                books = torch.cat(
+                    [books, book.view(1, self.book_size, latent_ndim)], dim=0
+                )
+
+            indices = torch.argmax(logits, dim=2).unsqueeze(2)
+            encodings = torch.zeros(
+                indices.shape[0],
+                indices.shape[1],
+                self.book_size,
+                device=indices.device,
+            )
+            encodings.scatter_(2, indices, 1)
+            zq = torch.matmul(encodings, books)
+            # mean_prob = torch.mean(encodings, dim=0)
+        # zq (b, npts, latent_ndim)
+
+        prob = torch.softmax(logits, dim=-1)
+        prob = prob.view(b, n_pts, self.book_size)
+        log_prob = torch.log_softmax(logits, dim=-1)
+        log_prob = log_prob.view(b, n_pts, self.book_size)
+
+        return zq, precision_q, prob, log_prob
+
+
+class Reconstruction(nn.Module):
+    def __init__(self, config, n_pts):
+        super().__init__()
+        self.seq_len = config.seq_len
+        self.n_pts = n_pts
+        self.latent_ndim = config.latent_ndim
+        self.conv1 = nn.Sequential(
+            nn.ConvTranspose1d(config.latent_ndim, config.latent_ndim // 2, 3, 1),
+            nn.GroupNorm(1, config.latent_ndim // 2),
+            nn.SiLU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.ConvTranspose1d(config.latent_ndim // 2, config.latent_ndim // 4, 5, 2),
+            nn.GroupNorm(1, config.latent_ndim // 4),
+            nn.SiLU(),
+        )
+        self.conv3 = nn.Sequential(
+            nn.ConvTranspose1d(config.latent_ndim // 4, config.latent_ndim // 8, 5, 3),
+            nn.GroupNorm(1, config.latent_ndim // 8),
+            nn.SiLU(),
+        )
+        self.conv4 = nn.Sequential(
+            nn.ConvTranspose1d(config.latent_ndim // 8, 2, 6, 3),
+            # nn.GroupNorm(1, seq_len),
+            nn.Tanh(),
+        )
+
+    def forward(self, zq):
+        # zq (b, n_pts, ndim)
+        zq = zq.reshape(-1, self.latent_ndim, 1)  # (b * n_pts, ndim, 1)
+        zq = self.conv1(zq)
+        zq = self.conv2(zq)
+        zq = self.conv3(zq)
+        x = self.conv4(zq)
+        # x (b * n_pts, 2, seq_len)
+        x = x.view(-1, self.n_pts, 2, self.seq_len)
+        x = x.permute(0, 3, 1, 2)
+        # x (b, seq_len, n_pts, 2)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, config: SimpleNamespace):
+        super().__init__()
+        self.encoders = nn.ModuleList(
+            [
+                TransformerEncoderBlock(
+                    config.latent_ndim, config.nheads, config.dropout
+                )
+                for _ in range(config.nlayers)
+            ]
+        )
+        # self.recon = Reconstruction(config)
+        self.recon_kps = Reconstruction(config, 13)
+        self.recon_bbox = Reconstruction(config, 2)
+
+    def forward(self, zq):
+        # zq (b, n_pts, latent_ndim)
+        for layer in self.encoders:
+            zq, attn_w = layer(zq)
+
+        # x = self.recon(zq)
+        # recon_kps, recon_bbox = x[:, :, :-2], x[:, :, -2:]
+        recon_kps = self.recon_kps(zq[:, :13, :])
+        recon_bbox = self.recon_bbox(zq[:, 13:, :])
+
+        return recon_kps, recon_bbox
---
+++ src/model/individual/sqvae_todo.py
@@ -0,0 +1,278 @@
+from types import SimpleNamespace
+from typing import Optional
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from lightning.pytorch import LightningModule
+from numpy.typing import NDArray
+
+# TODO
+from src.model.individual.modules_todo import (
+    ClassificationHead,
+    Decoder,
+    Encoder,
+    GaussianVectorQuantizer,
+    gumbel_softmax_relaxation,
+)
+
+
+class SQVAE(LightningModule):
+    def __init__(self, config: SimpleNamespace, annotations: Optional[NDArray] = None):
+        super().__init__()
+        self.config = config
+        self.latent_ndim = config.latent_ndim
+
+        self.n_clusters = config.n_clusters
+        self.temp_cls_init = config.temp_cls_init
+        self.temp_cls_decay = config.temp_cls_decay
+        self.temp_cls_min = config.temp_cls_min
+        self.temperature_cls = None
+
+        self.temp_init = config.temp_init
+        self.temp_decay = config.temp_decay
+        self.temp_min = config.temp_min
+        self.temperature = None
+
+        self.cls_head = None
+        self.encoder = None
+        self.decoder = None
+        self.quantizer = None
+        self.annotations = annotations
+
+    def configure_model(self):
+        if self.encoder is not None:
+            return
+        self.cls_head = ClassificationHead(self.config)
+        self.encoder = Encoder(self.config)
+        self.decoder = Decoder(self.config)
+        self.quantizer = GaussianVectorQuantizer(self.config)
+
+    def configure_optimizers(self):
+        opt = torch.optim.AdamW(self.parameters(), self.config.lr, self.config.betas)
+        sch = torch.optim.lr_scheduler.ExponentialLR(opt, self.config.lr_gamma)
+        # sch = torch.optim.lr_scheduler.CosineAnnealingLR(
+        #     opt, self.config.t_max, self.config.lr_min
+        # )
+        return [opt], [sch]
+        # return opt
+
+    def forward(self, kps, bbox, is_train):
+        # kps (b, seq_len, n_pts, 2)
+        # bbox (b, seq_len, n_pts, 2)
+
+        # classification
+        c_logits, attn_w_cls = self.cls_head(kps, bbox, is_train)
+        if is_train:
+            c_probs = torch.zeros_like(c_logits)
+            for i, c_logit in enumerate(c_logits):
+                c_prob = gumbel_softmax_relaxation(c_logit, self.temperature_cls)
+                c_probs[i] = c_prob
+        else:
+            c_probs = F.softmax(c_logits, dim=-1)
+        # c_prob (b, n_clusters)
+
+        # encoding
+        ze, attn_w = self.encoder(kps, bbox, c_probs, is_train)
+        # ze (b, npts, latent_ndim)
+
+        # quantization
+        zq, precision_q, prob, log_prob = self.quantizer(
+            ze, c_probs, self.temperature, is_train
+        )
+        # zq (b, npts, latent_ndim)
+        # prob (b, npts, book_size)
+
+        # reconstruction
+        recon_kps, recon_bbox = self.decoder(zq)
+
+        if is_train:
+            c_probs = F.softmax(c_logits, dim=-1)
+        return (
+            ze,
+            zq,
+            attn_w,
+            attn_w_cls,
+            precision_q,
+            prob,
+            log_prob,
+            recon_kps,
+            recon_bbox,
+            c_probs,
+        )
+
+    def calc_temperature(self, temp_init, temp_decay, temp_min):
+        return np.max(
+            [
+                temp_init * np.exp(-temp_decay * self.global_step),
+                temp_min,
+            ]
+        )
+
+    def mse_x(self, x, recon_x, reduction):
+        mse = F.mse_loss(recon_x, x, reduction="none")
+        if reduction == "mean":
+            return mse.mean(dim=(1, 2, 3))  # (b,)
+        elif reduction == "sum":
+            return mse.sum(dim=(1, 2, 3))  # (b,)
+        else:
+            return mse  # (b,)
+
+    def loss_x(self, x, recon_x):
+        mses = self.mse_x(x, recon_x, "sum")
+        return mses.mean()
+
+    def loss_kl_continuous(self, ze, zq, precision_q):
+        return torch.sum(((ze - zq) ** 2) * precision_q, dim=(1, 2)).mean()
+
+    def loss_kl_discrete(self, prob, log_prob):
+        return torch.sum(prob * log_prob, dim=1).mean()
+
+    def process_batch(self, batch):
+        keys, ids, kps, bbox, mask = batch
+        keys = np.array(keys).ravel()
+        if kps.device != self.device:
+            kps = kps.to(self.device)
+            bbox = bbox.to(self.device)
+        if kps.ndim == 5:
+            ids = ids[0]
+            kps = kps[0]
+            bbox = bbox[0]
+            # mask = mask[0]
+
+        return keys, ids, kps, bbox, mask
+
+    def training_step(self, batch, batch_idx):
+        keys, ids, kps, bbox, mask = self.process_batch(batch)
+
+        # update temperature of gumbel softmax
+        self.temperature_cls = self.calc_temperature(
+            self.temp_cls_init, self.temp_cls_decay, self.temp_cls_min
+        )
+        self.temperature = self.calc_temperature(
+            self.temp_init, self.temp_decay, self.temp_min
+        )
+
+        # forward
+        (
+            ze,
+            zq,
+            attn_w,
+            attn_w_cls,
+            precision_q,
+            prob,
+            log_prob,
+            recon_kps,
+            recon_bbox,
+            c_prob,
+        ) = self(kps, bbox, True)
+
+        # ELBO loss
+        lrc_kps = self.loss_x(kps, recon_kps)
+        lrc_bbox = self.loss_x(bbox, recon_bbox)
+        kl_continuous = self.loss_kl_continuous(ze, zq, precision_q)
+        kl_discrete = self.loss_kl_discrete(prob, log_prob)
+        loss_dict = dict(
+            kps=lrc_kps.item(),
+            bbox=lrc_bbox.item(),
+            kl_discrete=kl_discrete.item(),
+            kl_continuous=kl_continuous.item(),
+            log_param_q=self.quantizer.log_param_q.item(),
+            log_param_q_cls=self.cls_head.log_param_q_cls.item(),
+        )
+
+        # clustering loss
+        c_prior = torch.full_like(c_prob, 1 / self.n_clusters)
+        c_prob = torch.clamp(c_prob, min=1e-10)
+        lc_psuedo = F.kl_div(c_prob.log(), c_prior)
+        loss_dict["c_psuedo"] = lc_psuedo.item()
+
+        if self.annotations is not None:
+            keys = np.array(["{}_{}".format(*key.split("_")[0::2]) for key in keys])
+            mask_supervised = np.isin(keys, self.annotations.T[0]).ravel()
+            keys = keys[mask_supervised]
+            mask_supervised = torch.tensor(mask_supervised).to(self.device)
+            if torch.any(mask_supervised):
+                labels = []
+                for key in keys:
+                    if key in self.annotations.T[0]:
+                        label = self.annotations.T[1][key == self.annotations.T[0]]
+                        labels.append(int(label))
+                labels = torch.tensor(labels).to(self.device, torch.long)
+                lc_real = F.cross_entropy(
+                    c_prob[mask_supervised], labels, reduction="sum"
+                )
+                lc_real = lc_real / ids.size(0)
+                loss_dict["c_real"] = lc_real.item()
+            else:
+                lc_real = 0.0
+                loss_dict["c_real"] = 0.0
+        else:
+            lc_real = 0.0
+
+        lc = lc_real + lc_psuedo * self.config.alpha_c
+
+        if self.current_epoch < 5:
+            loss_total = (
+                (lrc_kps + lrc_bbox) * 0.00001
+                + kl_continuous * 0.00001
+                + kl_discrete * 0.00001
+                + lc * 1
+            )
+        else:
+            loss_total = (
+                (lrc_kps + lrc_bbox) * self.config.lmd_lrc
+                + kl_continuous * self.config.lmd_klc
+                + kl_discrete * self.config.lmd_kld
+                + lc * self.config.lmd_c
+            )
+        loss_dict["total"] = loss_total.item()
+
+        self.log_dict(loss_dict, prog_bar=True, logger=True)
+
+        return loss_total
+
+    @torch.no_grad()
+    def predict_step(self, batch):
+        keys, ids, kps, bbox, mask = self.process_batch(batch)
+
+        # forward
+        (
+            ze,
+            zq,
+            attn_w,
+            attn_w_cls,
+            precision_q,
+            prob,
+            log_prob,
+            recon_kps,
+            recon_bbox,
+            c_prob,
+        ) = self(kps, bbox, False)
+
+        mse_kps = self.mse_x(kps, recon_kps, "mean")
+        mse_bbox = self.mse_x(bbox, recon_bbox, "mean")
+
+        results = []
+        for i in range(len(keys)):
+            data = {
+                "key": keys[i],
+                "id": ids[i].cpu().numpy().item(),
+                "kps": kps[i].cpu().numpy(),
+                "recon_kps": recon_kps[i].cpu().numpy(),
+                "mse_kps": mse_kps[i].item(),
+                "bbox": bbox[i].cpu().numpy(),
+                "recon_bbox": recon_bbox[i].cpu().numpy(),
+                "mse_bbox": mse_bbox[i].item(),
+                "ze": ze[i].cpu().numpy(),
+                "zq": zq[i].cpu().numpy(),
+                "attn_w": attn_w[i].cpu().numpy(),
+                "attn_w_cls": attn_w_cls[i].cpu().numpy(),
+                "book_prob": prob[i].cpu().numpy(),
+                "book_idx": prob[i].cpu().numpy().argmax(axis=1),
+                "label_prob": c_prob[i].cpu().numpy(),
+                "label": c_prob[i].cpu().numpy().argmax(),
+            }
+            results.append(data)
+
+        return results
