3599a8da470ab530e6b84a55d7ca7d1643fbb649
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-12-16 13:05:20 +0900

    update individual model

--- scripts/train_individual_multiple_videos.py
+++ scripts/train_individual_multiple_videos.py
@@ -13,7 +13,7 @@ sys.path.append(".")
 from src.data import individual_train_dataloader, load_annotation_train
 
 # TODO
-from model.individual.sqvae import CSQVAE
+from model.individual.csqvae import CSQVAE
 from src.utils import yaml_handler
 
 if __name__ == "__main__":
--- src/model/individual/__init__.py
+++ src/model/individual/__init__.py
@@ -1,2 +1,2 @@
 from .vae import VAE
-from .sqvae import SQVAE
+from .csqvae import CSQVAE
